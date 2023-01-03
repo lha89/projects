94ab79d2a323bda16444258126a84f2be1bf895d
Long Ha <longha89@gmail.com>
2023-01-02 21:53:28 -0700

    final

--- passwordGen.py
+++ passwordGen.py
@@ -1,5 +1,33 @@
 # The purpose of this code is to produce a password generator
 
-# test
+# Import modules
+import secrets, string
 
-#test 2
+# Define the data
+letters = string.ascii_letters
+numbers = string.digits
+symbols = string.punctuation
+
+final_pwd = letters + numbers + symbols
+
+# Define password length
+pwd_length = 10
+
+# Generate a password
+pwd = ''
+for i in range(pwd_length):
+    pwd += ''.join(secrets.choice(final_pwd))
+
+print(pwd)
+
+# Ensure that restrictions apply
+while True:
+    pwd = ''
+    for i in range(pwd_length):
+        pwd += ''.join(secrets.choice(final_pwd))
+        
+    if (any(char in symbols for char in pwd) and
+        sum(char in numbers for char in pwd) >=6):
+        break
+
+print(pwd)
